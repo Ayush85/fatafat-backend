3e66bc2634dcdbec70432a96980a8aaff11ab1b3
devlekhraj <devlekhraj88@gmail.com>
2026-06-02 01:12:55 +0545

    CartItem model imported in Cart.php model

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use App\Models\CartItem;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
 class Cart extends Model
 {
